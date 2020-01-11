815738590f16ba3aa89b2bce64f64909217c41c4
k-young-passionate <5kyoungs@skku.edu>
2020-01-11 16:42:48 +0900

    code comment updated

--- clietCode/cs.py
+++ clietCode/cs.py
@@ -1,13 +1,17 @@
 import requests
 
 
+'''
+@author: k-young-passionate
+@params:
+		addUrl: url 뒤에 추가할 경로
+		params: 서버에 body에 넣어 보낼 값 map 으로 전달
+		conType: GET 방식과 POST 방식 구분, default는 GET
+@return:  (response code, response message) 
+'''
+
 def getRequest(addUrl="", params="", conType="GET"):
   URL = 'http://211.254.215.243:18070/' + addUrl
-
-#f = open('./cpimg.jpeg', 'rb')
-#img = f.read()
-#f.close()
-#params = {'hi' : 'hi2', 'hello': img}
 
   if conType == "GET":
     res = requests.get(URL, data=params)
@@ -21,4 +25,8 @@ def getRequest(addUrl="", params="", conType="GET"):
 
 # TEST CODE
 # c = {'hi' : 'asdf'}
+# f = open('./test.jpeg', 'rb')
+# img = f.read()
+# f.close()
+# params = {'hi' : 'hi2', 'hello': img}
 # print(getRequest('ih', c, "POST"))
