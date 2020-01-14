ca99946b0aef58e49871c11d4916bdc1c5d1e9e8
lja9702 <lja9702@naver.com>
2020-01-14 15:03:03 +0900

    Update detect_shape.py:
    - model: output_graph.ph
    - label: output_labels.txt
    - inceptionV3모델을 사용한 코드 추가: inceptionv3_face_shape

--- homepage/detect_shape.py
+++ homepage/detect_shape.py
@@ -4,6 +4,10 @@ import dlib
 import glob
 import cv2  #opencv 사용
 import math
+
+import numpy as np
+import tensorflow.compat.v1 as tf
+
 from flask import Blueprint, request, render_template, flash, redirect, url_for
 from flask import current_app as current_app
 
@@ -18,7 +22,12 @@ class DetectShape:
         # 랜드마크 파일 경로
         self.predictor_path = "static/model/shape_predictor_68_face_landmarks.dat"
         # 이미지 경로
-        self.img_path = "./pimg/capture"+str(index)+".jpg"
+        self.img_path = "static/img/capture.jpg"
+        # self.img_path = "./pimg/capture"+str(index)+".jpg"
+
+        #TODO: self.model_full_path와 labesFullPath 변경
+        self.model_full_path = 'static/model/output_graph.pb'                                      # 읽어들일 graph 파일 경로
+        self.labels_full_path = 'static/img/output_labels.txt'                                   # 읽어들일 labels 파일 경로
         #랜드마크 포인트 리스트
         self.landmark_list = []
         self.shape = ""
@@ -125,6 +134,46 @@ class DetectShape:
         else:
             self.shape = "tri"
 
-# test = DetectShape()
-# test.measure_face_shape()
-# print(test.shape)
+
+    def create_graph(self):
+        """저장된(saved) GraphDef 파일로부터 graph를 생성하고 saver를 반환한다."""
+        # 저장된(saved) graph_def.pb로부터 graph를 생성한다.
+        with tf.gfile.FastGFile(self.model_full_path, 'rb') as f:
+            graph_def = tf.GraphDef()
+            graph_def.ParseFromString(f.read())
+            _ = tf.import_graph_def(graph_def, name='')
+
+    def inceptionv3_face_shape(self):
+        answer = None
+
+        if not tf.gfile.Exists(self.img_path):
+            tf.logging.fatal('File does not exist %s', self.img_path)
+            return answer
+
+        image_data = tf.gfile.FastGFile(self.img_path, 'rb').read()
+
+        # 저장된(saved) GraphDef 파일로부터 graph를 생성한다.
+        self.create_graph()
+
+        with tf.Session() as sess:
+
+            softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+            predictions = sess.run(softmax_tensor,
+                                   {'DecodeJpeg/contents:0': image_data})
+            predictions = np.squeeze(predictions)
+
+            top_k = predictions.argsort()[-5:][::-1]  # 가장 높은 확률을 가진 5개(top 5)의 예측값(predictions)을 얻는다.
+            f = open(self.labels_full_path, 'rb')
+            lines = f.readlines()
+            labels = [str(w).replace("\n", "") for w in lines]
+            for node_id in top_k:
+                human_string = labels[node_id]
+                score = predictions[node_id]
+                print('%s (score = %.5f)' % (human_string, score))
+
+            answer = labels[top_k[0]]
+            test.shape = answer[2:-3]
+
+test = DetectShape(1)
+test.inceptionv3_face_shape()
+print(test.shape)
