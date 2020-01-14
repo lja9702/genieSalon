b95d24f6971ab18478354d8768dbfae6bd741088
JinAh Lee <lja9702@naver.como>
2020-01-14 23:18:46 +0900

    raspCode Upload

---
+++ raspCode/user_auth.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Example 2: STT - getVoice2Text """
+
+from __future__ import print_function
+
+import grpc
+import os
+import datetime
+import hmac
+import hashlib
+
+
+# Config for GiGA Genie gRPC
+CLIENT_ID = 'Y2xpZW50X2lkMTU3ODk2ODg3MzczNg=='
+CLIENT_KEY = 'Y2xpZW50X2tleTE1Nzg5Njg4NzM3MzY='
+CLIENT_SECRET = 'Y2xpZW50X3NlY3JldDE1Nzg5Njg4NzM3MzY='
+
+### COMMON : Client Credentials ###
+def getMetadata():
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S%f")[:-3]
+    message = CLIENT_ID + ':' + timestamp
+
+    signature = hmac.new(CLIENT_SECRET.encode(), message.encode(), hashlib.sha256).hexdigest()
+
+    metadata = [('x-auth-clientkey', CLIENT_KEY),
+                ('x-auth-timestamp', timestamp),
+                ('x-auth-signature', signature)]
+
+    return metadata
+
+def credentials(context, callback):
+    callback(getMetadata(), None)
+
+def getCredentials():
+    with open('../data/ca-bundle.pem', 'rb') as f:
+        trusted_certs = f.read()
+    sslCred = grpc.ssl_channel_credentials(root_certificates=trusted_certs)
+
+    authCred = grpc.metadata_call_credentials(credentials)
+
+    return grpc.composite_channel_credentials(sslCred, authCred)
+
+### END OF COMMON ###
